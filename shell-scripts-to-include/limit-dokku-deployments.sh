d5956a734e0cbcf53eaa50c05c21d73058221bb8
Fredrik Wollsén <fredrik@neam.se>
2014-05-22 18:33:52 +0200

    Improved and generalized limit-dokku-deployments.sh

--- shell-scripts-to-include/limit-dokku-deployments.sh
+++ shell-scripts-to-include/limit-dokku-deployments.sh
@@ -1,30 +1,37 @@
 #!/bin/bash
 
-# usage: ./limit-dokku-deployments.sh 3 "project1 project2 project3 project4"
+# Usage: ./limit-dokku-deployments.sh <limit> <regex>
+
+# Kills all but the <limit> most recent containers that matches the <regex> from the output of "docker ps"
+# Use to kill off older deployments in the case that your CI or similar
+# is automatically adding new commit-specific deployments
+
+# Example: ./limit-dokku-deployments.sh 3 'dokku/travis'
 
 set -x
 
 # config
 
 LIMIT=$1
+GREPARGS=${@:2}
 
 # logic
 
-for PROJECT in $2
-do
+RUNNING=`docker ps | grep $GREPARGS | wc | awk '{ print $1 }'`
+let KILL=RUNNING-LIMIT
 
-    RUNNING=`docker ps | grep "\-$PROJECT\-" | wc | awk '{ print $1 }'`
-    let KILL=RUNNING-LIMIT
+echo "$RUNNING containers matching regex \"$GREPARGS\""
+docker ps | grep $GREPARGS
+echo
 
-    echo "$RUNNING commit-specific containers active for project $PROJECT"
+if [ "$KILL" -gt 0 ] ; then
 
-    if [ "$KILL" -gt 0 ] ; then
+    # kill all but the LIMIT newest deployments within each project
+    docker ps | grep $GREPARGS | awk '{ print $1 }' | tail -n "$KILL" | xargs docker kill
 
-        # kill all but the LIMIT newest deployments within each project
-        docker ps | grep "\-$PROJECT\-" | awk '{ print $1 }' | tail -n "$KILL" | xargs docker kill
+    # perform necessary clean-up for the apps be able to rebuild
+    `dirname $0`/remove-cid-files-that-dont-have-active-containers.sh
 
-    fi
-
-done
+fi
 
 exit 0
