8b479fe620a25be1af0f908019e634626b05353d
Fredrik Wollsén <fredrik@neam.se>
2014-09-29 12:06:34 +0200

    More aggressive remove phantom containers script also removes stopped/killed containers

--- shell-scripts-to-include/remove-phantom-docker-images-and-containers.sh
+++ shell-scripts-to-include/remove-phantom-docker-images-and-containers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker ps | grep -v mariadb | grep -v progrium | grep -v ubuntu | grep -v CONTAINER | awk '{ print $1 }' | xargs docker rm -f
-docker ps -a | grep Exited | grep -v CONTAINER | awk '{ print $1 }' | xargs docker rm -f
+docker ps -a | grep -v dokku | grep -v mariadb | grep -v progrium | grep -v ubuntu| grep -v CONTAINER | awk '{ print $1 }' | xargs docker rm -f
 docker images | awk '{ print $3 }' | xargs docker rmi
 docker images | grep '<none>' | awk '{ print $3 }' | xargs docker rmi -f
 
