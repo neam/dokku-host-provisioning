44b5118b01b221b966675e03ec9380ccb904a4de
Fredrik Wollsén <fredrik@neam.se>
2014-09-29 03:05:12 +0200

    Removed sync directory contents before copying the latest

--- build-vagrant-config.sh
+++ build-vagrant-config.sh
@@ -6,7 +6,9 @@ initial_pwd=$(pwd)
 script_path=`dirname $0`
 
 erb ../../Vagrantfile.erb > Vagrantfile
+rm -r shell-scripts-to-include/
 cp -r ../../shell-scripts-to-include/ shell-scripts-to-include/
+rm -r vendor/
 cp -r ../../vendor/ vendor/
 erb ../../provision.sh.erb > provision.sh
 
